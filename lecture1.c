5e3cdfeab886352e57d12066d45b12243623b412
nikolajjsj <nikolajjsj@gmail.com>
2020-03-28 09:01:15 +0100

    fixed the previous function with '/n' tag

--- lecture1.c
+++ lecture1.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+// First function to print: "Hello, World!", but stays on same line..
 int main(void)
 {
     printf('Hello, World!');
+}
+
+// to fix use this tag "/n"
+int main(void)
+{
+    printf('Hellow, World!/n');
 }
